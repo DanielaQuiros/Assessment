5ed8200cd91a8a5405f4ba1cd3f416316cd1aa7b
DanielaQuiros <42482085+DanielaQuiros@users.noreply.github.com>
2024-02-23 21:45:42 -0600

    Create 4-sp_clean_data.sql

---
+++ 4-sp_clean_data.sql
@@ -0,0 +1,26 @@
+/**********************************************************
+** Store procedure to clean data from invalid characters **
+**********************************************************/
+
+
+create or replace procedure sp_clean_data()
+returns string
+language SQL
+as
+$$
+begin
+    update unique_transactions
+    -- Replaces special characters
+    set client_name = translate(client_name,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        client_lastname = translate(client_lastname,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        store_name = translate(store_name,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        location = translate(location,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        product_name = translate(product_name,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        category = translate(category,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        brand = translate(brand,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        street = translate(street,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        city = translate(city,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu'),
+        state = translate(state,'ÑñÁáÉéÍíÓóÚú','NnAaEeIiOoUu');
+return 'SUCCESS';
+end;
+$$;
