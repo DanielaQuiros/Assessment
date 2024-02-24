6941792a07ff14f8b62f71d059ac26fa619d9a8f
DanielaQuiros <42482085+DanielaQuiros@users.noreply.github.com>
2024-02-23 21:34:25 -0600

    Create 2-sp_deleteduplicates.sql

---
+++ 2-sp_deleteduplicates.sql
@@ -0,0 +1,42 @@
+/******************************************
+** Stored procedure to delete duplicates **
+******************************************/
+
+
+create or replace procedure sp_deleteduplicates()
+    returns string
+    language SQL
+as
+$$
+begin
+    --Inserting unique results in a staging table
+    create or replace table unique_transactions as 
+    select * from (
+        select *,
+        row_number() over(partition by client_id,
+                                       client_name,
+                                       client_lastname, 
+                                       email,
+                                       store_id,
+                                       store_name,
+                                       location,
+                                       product_id,
+                                       product_name,
+                                       category,
+                                       brand,
+                                       address_id,
+                                       street,
+                                       city,
+                                       state,
+                                       zip_code,
+                                       transaction_id,
+                                       quantity_of_items_sold,
+                                       unit_price,
+                                       discount
+        order by client_id) as rn
+        from transactions
+    )
+    where rn = 1; -- Filter first occurrence of each record
+    return 'Success';
+end;
+$$;
