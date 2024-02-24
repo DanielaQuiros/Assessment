1ad9a63a0c0897ba88008bc65cd69e6ef9a2c2fe
DanielaQuiros <42482085+DanielaQuiros@users.noreply.github.com>
2024-02-23 21:54:19 -0600

    Create 7-sp_load_product.sql

---
+++ 7-sp_load_product.sql
@@ -0,0 +1,21 @@
+/********************************************************
+** Store procedure to load the product dimension table **
+********************************************************/
+
+
+create or replace procedure sp_load_product()
+returns string
+language SQL
+as
+$$
+begin
+
+    insert into Product
+    select distinct product_id,
+           product_name,
+           category,
+           brand
+    from unique_transactions;
+    return 'SUCCESS';
+end;
+$$;
