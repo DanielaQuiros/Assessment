14cfd03bfa23f2e2f573427bafe23cd20cc48ef1
DanielaQuiros <42482085+DanielaQuiros@users.noreply.github.com>
2024-02-23 21:58:01 -0600

    Create 9-sp_load_sales.sql

---
+++ 9-sp_load_sales.sql
@@ -0,0 +1,24 @@
+/*********************************************
+** Store procedure to load sales fact table **
+*********************************************/
+
+
+create or replace procedure sp_load_sales()
+returns string
+language SQL
+as
+$$
+begin
+    insert into Sales
+    select transaction_id,
+    quantity_of_items_sold,
+    unit_price,
+    discount,
+    client_id,
+    store_id,
+    product_id,
+    address_id
+    from unique_transactions;
+    return 'SUCCESS';
+end;
+$$;
