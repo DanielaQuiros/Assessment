60d5d66ad2121e3588168024b057bd47fd4a47aa
DanielaQuiros <42482085+DanielaQuiros@users.noreply.github.com>
2024-02-23 21:55:39 -0600

    Create 8-sp_load_address.sql

---
+++ 8-sp_load_address.sql
@@ -0,0 +1,21 @@
+/********************************************************
+** Store procedure to load the address dimension table **
+********************************************************/
+
+
+create or replace procedure sp_load_address()
+returns string
+language SQL
+as
+$$
+begin
+insert into Address
+select distinct address_id,
+       street,
+       city,
+       state,
+       zip_code
+from unique_transactions;
+return 'SUCCESS';
+end;
+$$;
